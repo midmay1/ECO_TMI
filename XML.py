6727b63660121902d6a56fa9337a56cb826294cf
song-eunsub <midmay1@gmail.com>
2020-10-12 21:31:51 +0900

    o

--- XML.py
+++ XML.py
@@ -5,22 +5,22 @@ import sys
 path = './SQL_XML'
 OA = os.path.join(path, 'Overall_toxicity.xml')
 
-# xml_data = ET.parse(OA).getroot()
+#xml_data = ET.parse(OA).getroot()
 
 
 n = 0
-
+JHead = "<RECORDS>"
+JTail = "</RECORDS>"
 with open(OA) as f:
     while n < 166:
         Head = f.readline()
-        # print(Test)
-        if Test.strip() == "<RECORD>":
-            n = n + 1
-            sys.stdout = open("excel.%d.xml" % n, "w")
-            for i in range(1, 40):
-                print(Head)
+        #print(Head)
+        if Head.strip() == "<RECORD>":
+            n = n+1
+            sys.stdout = open("excel.%d.xml" %n,"w")
+            print(JHead)
+            print(Head)
+            for i in range(1,40):
                 Test = f.readline()
                 print(Test)
-#test
-# print(head)
-# print(Test)
+            print(JTail)
