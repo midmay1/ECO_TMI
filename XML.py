4e6887a4dc7795e1738e3bb800505bc8671ada94
song-eunsub <midmay1@gmail.com>
2020-10-12 21:09:54 +0900

    o

--- XML.py
+++ XML.py
@@ -1,33 +1,26 @@
 import xml.etree.ElementTree as ET
 import os
+import sys
 
 path = './SQL_XML'
 OA = os.path.join(path, 'Overall_toxicity.xml')
 
-xml_data = ET.parse(OA).getroot()
-Tag_list = []
-C = 0
-for TA in xml_data.findall('./RECORD/'):
-    if C < 38:
-        #        print(TA.tag)
-        Tag_list.append(TA.tag)
-    C = C + 1
+# xml_data = ET.parse(OA).getroot()
 
-# print(Tag_list)
-print(len(Tag_list))
-nTag = int(len(Tag_list))
 
-Info = []
-i = 0
-while i <= nTag:
-    for TA in xml_data.iter(Tag_list[i]):
-        # if TA.tag == Tag_list[i]:
-        #    Tag_name = Tag_list[i]
-        # Info.append(TA.text)
+n = 0
 
-        print(TA.text)
-    i = i + 1
-
-# print(Info)
-
-## dictionary
+with open(OA) as f:
+    while n < 166:
+        Head = f.readline()
+        # print(Test)
+        if Test.strip() == "<RECORD>":
+            n = n + 1
+            sys.stdout = open("excel.%d.xml" % n, "w")
+            for i in range(1, 40):
+                print(Head)
+                Test = f.readline()
+                print(Test)
+#test
+# print(head)
+# print(Test)
