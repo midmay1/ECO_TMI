5fd78b479f2b538f651c0f94b34216b2e5ce40e6
jongho <cjh19940724@gmail.com>
2020-10-16 16:24:30 +0900

    xml metadata reading

--- xml_metadata.py
+++ xml_metadata.py
@@ -3,12 +3,14 @@ import re
 import html
 import xml.etree.ElementTree as ET
 
-xml_folder = os.path.join(db_folder, 'PUBCHEM', 'XML')
+db_folder = './V1_DATABASE/'
+xml_folder = os.path.join(db_folder, 'PUBCHEM_xml')
 cid_list = sorted([int(d.split('.')[-2]) for d \
                    in os.listdir(xml_folder) if '.xml' in d])
 
+list_all=['toxic list:\n']
 for cid in cid_list :
- xml_file = os.path.join(db_folder, 'PUBCHEM', 'XML', 'output.{}.xml'.format(cid))
+ xml_file = os.path.join(db_folder, 'PUBCHEM_xml', 'output.{}.xml'.format(cid))
 
  xml_data = ET.parse(xml_file).getroot()
  str_data = ET.tostring(xml_data, encoding='utf8').decode('utf8')
@@ -19,4 +21,4 @@ for cid in cid_list :
 
  toxic_list = regex.findall(str_data)
  toxic_list = [html.unescape(i.replace(start_str, '').replace(end_str, '')) for i in toxic_list]
-print(toxic_list)
+
